c70748a6ab1fb4731b322063f8db748c99f5495b
ikmemecoin-crypto <ikmemecoin@gmail.com>
2026-02-17 00:48:48 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,51 +1,17 @@
 import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
 
-title = "Fashion Bunny"
+df = pd.read_csv('sales_data.csv')
 
-st.title(title)
+st.title('Sales Data Dashboard')
 
-nav = st.container()
-with nav:
-    col1, col2, col3 = st.columns(3)
-    with col1:
-        st.write("Home")
-    with col2:
-        st.write("Products")
-    with col3:
-        st.write("About")
+st.write(df.head())
 
-header = st.container()
-with header:
-    st.image("kids_garments.jpg")
-    st.button("Shop Now")
+fig, ax = plt.subplots()
+ax.plot(df['Date'], df['Sales'])
+ax.set_xlabel('Date')
+ax.set_ylabel('Sales')
+st.pyplot(fig)
 
-section1 = st.container()
-with section1:
-    st.write("Featured Products")
-    col1, col2 = st.columns(2)
-    with col1:
-        st.image("product1.jpg")
-        st.write("$10.99")
-        st.button("Add to Cart")
-    with col2:
-        st.image("product2.jpg")
-        st.write("$9.99")
-        st.button("Add to Cart")
-
-section2 = st.container()
-with section2:
-    st.write("Categories")
-    col1, col2, col3 = st.columns(3)
-    with col1:
-        st.write("Tops")
-    with col2:
-        st.write("Bottoms")
-    with col3:
-        st.write("Dresses")
-
-footer = st.container()
-with footer:
-    st.write("Contact Information: 1234567890, fashionbunny@example.com, 123 main st")
-    st.write("Social Media Links: Facebook, Instagram")
-    email = st.text_input("Newsletter Signup")
-    st.button("Signup")
+st.bar_chart(df['Sales'])
