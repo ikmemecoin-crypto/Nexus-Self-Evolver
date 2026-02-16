8787607636888b650708e795b4e94c1abfa58ca1
ikmemecoin-crypto <ikmemecoin@gmail.com>
2026-02-17 00:20:29 +0500

    Agent write

--- text_summarizer.py
+++ text_summarizer.py
@@ -1,25 +1,25 @@
 import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize, sent_tokenize
-
-def summarize_text(text, num_sentences):
+def summarize_text(text, summary_length=5):
     stop_words = set(stopwords.words('english'))
     words = word_tokenize(text.lower())
-    freq = {}
+    words = [word for word in words if word.isalpha() and word not in stop_words]
+    word_freq = {}
     for word in words:
-        if word not in stop_words:
-            if word not in freq:
-                freq[word] = 1
-            else:
-                freq[word] += 1
+        if word not in word_freq:
+            word_freq[word] = 1
+        else:
+            word_freq[word] += 1
     sentences = sent_tokenize(text)
     sentence_scores = {}
     for sentence in sentences:
         for word in word_tokenize(sentence.lower()):
-            if word in freq:
+            if word in word_freq:
                 if sentence not in sentence_scores:
-                    sentence_scores[sentence] = freq[word]
+                    sentence_scores[sentence] = word_freq[word]
                 else:
-                    sentence_scores[sentence] += freq[word]
-    summary_sentences = sorted(sentence_scores, key=sentence_scores.get, reverse=True)[:num_sentences]
-    return ' '.join(summary_sentences)
+                    sentence_scores[sentence] += word_freq[word]
+    summary_sentences = sorted(sentence_scores.items(), key=lambda x: x[1], reverse=True)[:summary_length]
+    summary = ' '.join([sentence for sentence, score in summary_sentences])
+    return summary
